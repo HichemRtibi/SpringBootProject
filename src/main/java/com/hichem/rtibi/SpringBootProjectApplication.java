74e3bf24564fe0162751727e3d94ba859edfd309
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-22 22:57:23 +0100

    first controller

--- src/main/java/com/hichem/rtibi/SpringBootProjectApplication.java
+++ src/main/java/com/hichem/rtibi/SpringBootProjectApplication.java
@@ -2,12 +2,26 @@ package com.hichem.rtibi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+
+	@RestController
+	@RequestMapping(value = "/")
+	public class TestController {
+		@RequestMapping(value = "hello/{name}", method = RequestMethod.GET)
+		public String hello(@PathVariable(value = "name") String name) {
+			return "hello " + name;
+		}
+
 	}
 
 }
